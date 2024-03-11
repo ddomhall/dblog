155b5d7f6e21742d551d11d1cb25819ddeba9884
ddomhall <domjshall@gmail.com>
2024-03-11 07:17:19 +0000

    comment

--- client/src/Comment.jsx
+++ client/src/Comment.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect, useContext} from 'react'
 import {useParams, Link} from 'react-router-dom'
 import {SessionContext} from './SessionContext.jsx'
+import formattedDate from '../utils/formattedDate.js'
 
 export default function Comment() {
   const [comment, setComment] = useState({content: '', author: {username: ''}, date: ''})
@@ -31,26 +32,28 @@ export default function Comment() {
   }
 
   return (
-    <div className='w-96 m-auto'>
+    <div className='w-96 m-auto ring ring-white rounded-xl p-2'>
       <section>
         {!edit ? 
-          <div>
+          <div className='flex flex-col gap-2'>
             <p>{comment.content}</p>
-            <Link to={'/users/' + comment.author._id}>{comment.author.username}</Link>
-            <p>{comment.date}</p>
+            <div className='flex justify-between'>
+            <Link to={'/users/' + comment.author._id} className='underline'>{comment.author.username}</Link>
+            <p>{formattedDate(comment.date)}</p>
+            </div>
           </div> :
-          <form onSubmit={editComment} className='flex flex-col'>
-            <input name='content' placeholder='content' />
-            <input type='submit' value='comment' />
+          <form onSubmit={editComment} className='flex flex-col gap-2'>
+            <input name='content' placeholder='content' className='rounded-xl px-2' />
+            <input type='submit' value='comment' className='ring ring-white rounded-xl px-2' />
           </form>
         }
         {session == comment.author._id ?
-          <div className='flex justify-between'>
+          <div className='flex justify-between mt-2'>
             {edit ?
-            <button onClick={() => setEdit(false)}>cancel</button> :
-            <button onClick={() => setEdit(true)}>edit</button>
+            <button onClick={() => setEdit(false)} className='ring ring-white rounded-xl px-2'>cancel</button> :
+            <button onClick={() => setEdit(true)} className='ring ring-white rounded-xl px-2'>edit</button>
             }
-            <button onClick={deleteComment}>delete</button>
+            <button onClick={deleteComment} className='ring ring-white rounded-xl px-2'>delete</button>
           </div> : ''
         }
       </section>
