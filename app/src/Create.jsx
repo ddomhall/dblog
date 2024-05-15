6b5521bf3b4bb08b6e990795ce37f211f6eed6af
ddomhall <domjshall@gmail.com>
2024-05-15 19:55:16 +0100

    create input ring

--- app/src/Create.jsx
+++ app/src/Create.jsx
@@ -1,8 +1,8 @@
-import {useContext} from 'react'
-import {SessionContext} from './SessionContext.jsx'
+import { useContext } from 'react'
+import { SessionContext } from './SessionContext.jsx'
 
 export default function create() {
-  const {session} = useContext(SessionContext)
+  const { session } = useContext(SessionContext)
 
   async function postApi(e) {
     e.preventDefault()
@@ -18,22 +18,22 @@ export default function create() {
         author: session,
       })
     }).then(res => res.json()).then(res => {
-        if (res.status == 200) {
-          window.location.replace('/')
-        } else {
-        }
-      })
+      if (res.status == 200) {
+        window.location.replace('/')
+      } else {
+      }
+    })
   }
 
 
   return (
-    <form onSubmit={postApi} className='flex flex-col w-80 m-auto gap-2 ring ring-white p-2 rounded-xl'>
-      <input name='content' placeholder='content' className='rounded-xl px-2'/>
+    <form onSubmit={postApi} className='flex flex-col w-80 m-auto gap-4 ring ring-white p-3 rounded-xl'>
+      <input name='content' placeholder='content' className='rounded-xl px-2 ring ring-white' />
       <div className='flex justify-between'>
-      <label htmlFor='published' className='pl-2'>published
-        <input name='published' id='published' type='checkbox' className='ml-2'/>
-      </label>
-      <input type='submit' value='post' className='ring ring-white rounded-xl px-2'/>
+        <label htmlFor='published' className='pl-2'>published
+          <input name='published' id='published' type='checkbox' className='ml-2' />
+        </label>
+        <input type='submit' value='post' className='ring ring-white rounded-xl px-2' />
       </div>
     </form>
   )
